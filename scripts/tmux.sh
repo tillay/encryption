193d072df81ee6432bb71f04c80286027430fae9
semblanceofsense <semblanceofsensegaming@gmail.com>
2025-01-21 13:17:43 -0700

    Fixed tmux (again)

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -2,6 +2,6 @@
 ENCRYPTION_DIR=/home/$USER
 tmux new-session -d bash
 tmux split-window -h bash
-tmux send -t 0:0.0 "python3 ${ENCRYPTION_DIR}/encryption/symmetric/discord.py listen" C-m
-tmux send -t 0:0.1 "python3 ${ENCRYPTION_DIR}/encryption/symmetric/discord.py send" C-m
+tmux send -t "{ENCRYPTION_DIR}:0" -t 0:0.0 "python3 ${ENCRYPTION_DIR}/encryption/symmetric/discord.py listen" C-m
+tmux send -t "{ENCRYPTION_DIR}:0.1" -t 0:0.1 "python3 ${ENCRYPTION_DIR}/encryption/symmetric/discord.py send" C-m
 tmux -2 attach-session -d
