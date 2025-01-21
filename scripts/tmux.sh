52a81941e97fc19c214682e1f9b515c10967ef7d
Tilley <tilleyy8@gmail.com>
2025-01-20 23:06:54 -0700

    Update tmux.sh

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+ENCRYPTION_DIR=/home/$USER
 tmux new-session -d bash
 tmux split-window -h bash
-tmux send -t 0:0.0 "python3 /home/semblanceofsense/bin/encryption/symmetric/discord.py listen" C-m
-tmux send -t 0:0.1 "python3 /home/semblanceofsense/bin/encryption/symmetric/discord.py send" C-m
+tmux send -t 0:0.0 "python3 ${ENCRYPTION_DIR}/encryption/symmetric/discord.py listen" C-m
+tmux send -t 0:0.1 "python3 ${ENCRYPTION_DIR}/encryption/symmetric/discord.py send" C-m
 tmux -2 attach-session -d
