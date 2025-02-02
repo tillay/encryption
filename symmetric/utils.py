3348a1ce80f6a51ae5f8aa6ba458842890b9c8f4
tillay8 <tilleyy8@gmail.com>
2025-02-01 21:50:48 -0700

    maybe fix problems?

--- symmetric/utils.py
+++ symmetric/utils.py
@@ -1,7 +1,7 @@
 import sys, subprocess, os, base64, random, string
-from Cryptodome.Util.Padding import pad, unpad
+from Crypto.Util.Padding import pad, unpad
 from hashlib import sha256
-from Cryptodome.Cipher import AES
+from Crypto.Cipher import AES
 
 PASSWORD_FILE = "/tmp/key"
 prefix_password = "@@"
