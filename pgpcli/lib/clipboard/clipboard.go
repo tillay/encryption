cdcd46ceaf92e69b63693465abe89bd5361cbc9f
Your Name <you@example.com>
2025-08-28 09:22:00 -0600

    debug

--- pgpcli/lib/clipboard/clipboard.go
+++ pgpcli/lib/clipboard/clipboard.go
@@ -57,7 +57,7 @@ func x11Read() (string, error) {
 func waylandRead() (string, error) {
     cmd := exec.Command("wl-paste")
     output, err := cmd.Output()
-	if err !- nil {
+	if err != nil {
 		return "", err
 	}
     returnString := string(output)
